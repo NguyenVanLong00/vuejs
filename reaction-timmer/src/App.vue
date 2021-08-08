396202d3b990a6be0741ba362124221febe01505
Nguyen Van Long <nguyenvanlong0x0@gmail.com>
2021-08-08 15:47:41 +0700

    init reaction timmer

---
+++ reaction-timmer/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <h1>Reactor timmer game</h1>
+
+  <div>
+    <button class="button" @click="startGame" :disabled="isPlaying">
+      Start game
+    </button>
+  </div>
+  <Block v-if="isPlaying" :delay="delay" @endGame="endGame" />
+  
+  <p>{{score}}</p>
+</template>
+
+<script>
+import Block from "./components/Block.vue";
+
+export default {
+  name: "App",
+  components: {
+    Block,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score:null
+    };
+  },
+  methods: {
+    startGame() {
+      this.isPlaying = true;
+      this.delay = Math.random() * 3000;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ reaction-timmer/src/components/Block.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class=" block " @click="stopTimer" v-show="showBlock"> 
+        click now
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Block",
+    props:['delay'],
+    data(){
+        return {
+            showBlock:false,
+            timer:null,
+            reactionTime:0,
+        }
+    },
+    mounted(){
+        setTimeout(() => {
+            this.showBlock = true;
+            this.startTimer();
+        }, this.delay);
+    },
+    methods:{
+        startTimer(){
+            this.timer = setInterval(()=>{
+                this.reactionTime +=10;
+            },10);
+        },
+        stopTimer(){
+            clearInterval(this.timer);
+            this.$emit('endGame',this.reactionTime);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .block {
+        width:20rem;
+        height: 10rem;
+        background: tomato;
+        border-radius: 5px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color:white;
+        margin:32px auto;
+        box-shadow: 3px 6px 4px rgba(0, 0, 0, 0.1);
+    }
+</style>
