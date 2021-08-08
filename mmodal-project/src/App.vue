40efac72fbbf3c700127c5c7f868dfbdbf82d6b0
Nguyen Van Long <nguyenvanlong0x0@gmail.com>
2021-08-08 12:33:41 +0700

    init vuecli

---
+++ mmodal-project/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1>{{ title }}</h1>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="togleModal"/>
+  </div>
+
+  <button @click="togleModal">show modal</button>
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      title: "My first Vue app :)",
+      header: "this is my app",
+      text: "this is text",
+      showModal: false,
+    };
+  },
+  methods: {
+    togleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  display: inline-block;
+  border-bottom: 1px solid rgb(90, 89, 89);
+  padding-bottom: 8px;
+}
+</style>
---
+++ mmodal-project/src/components/Modal.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{sale:theme === 'sale'}">
+            <h2>{{ header }} </h2>
+            <p>{{ text }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default ({
+    props: ['header','text','theme'],
+    methods:{
+        closeModal(){
+            this.$emit('close')
+        }
+    }
+})
+</script>
+
+
+<style scoped>
+    .modal {
+        width:400px;
+        padding:20px;
+        margin:120px auto;
+        background: white;
+        border-radius: 5px;
+    }
+    .backdrop {
+        position: fixed;
+        top:0;
+        background: rgba(0, 0, 0, 0.4);
+        width: 100vw;
+        height: 100vh;
+    }
+    .sale {
+        background-color: rgb(109, 18, 18);
+        color:white;
+    }
+
+</style>
