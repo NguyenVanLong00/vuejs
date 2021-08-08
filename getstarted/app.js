e49868fef856e331635c75ff6d5915ac579ec4a8
Nguyen Van Long <nguyenvanlong0x0@gmail.com>
2021-08-08 08:57:25 +0700

    vue basic sintax

--- getstarted/app.js
+++ getstarted/app.js
@@ -1,9 +1,33 @@
 const app = Vue.createApp({
-   data(){
-       return {
-           title: ' this is title'
-       }
-   }
+    data() {
+        return {
+            
+            showBook: true,
+            books:[
+                {title:'b1',price:'20',url:'#1',isFav:true},
+                {title:'b2',price:'40',url:'#2',isFav:true},
+                {title:'b3',price:'10',url:'#3',isFav:false},
+                {title:'b4',price:'60',url:'#4',isFav:false},
+            ],
+        }
+    },
+    methods: {
+        changeshow(title) {
+            this.title = title;
+        },
+        handleEvent(e) {
+            console.log(e.type);
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX;
+            this.y = e.offsetY;
+        }
+    },
+    computed:{
+        filteredBook(){
+            return this.books.filter(book => book.isFav);
+        }
+    }
 });
 
 app.mount('#app');
